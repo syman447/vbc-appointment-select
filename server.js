e88189a782168b6082f40e185c6509e2380b0c25
Sy Adamowsky <syadamowsky@gmail.com>
2021-02-10 21:50:36 -0500

    changing root name

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/appointment-select", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
