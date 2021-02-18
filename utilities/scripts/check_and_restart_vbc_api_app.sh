0c198057fed00748111ee0281e2433ad962f0a54
Sy Adamowsky <syadamowsky@gmail.com>
2021-02-18 00:35:15 -0500

    script for reference

---
+++ utilities/scripts/check_and_restart_vbc_api_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PID=$(~/.nvm/versions/node/v10.22.1/bin/pm2 pid vbc-appointment-select)
+~/.nvm/versions/node/v10.22.1/bin/pm2 describe vbc-appointment-select > /dev/null
+RUNNING=$?
+curl http://localhost:8080/api/v2/calendars > /dev/null
+CALL_SUCCESSFUL=$?
+
+if [ "${RUNNING}" -ne 0 ] || [ "${PID}" -eq 0 ] || [ "${CALL_SUCCESSFUL}" -ne 0 ]; then
+  cd ~/repositories/vbc-appointment-select; ~/.nvm/versions/node/v10.22.1/bin/pm2 start apps.json
+fi;
