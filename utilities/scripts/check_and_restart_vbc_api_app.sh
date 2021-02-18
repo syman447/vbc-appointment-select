c675e94cda48a5e32392237df1fa2ceddb69e4b4
Sy Adamowsky <syadamowsky@gmail.com>
2021-02-18 00:46:43 -0500

    silent script

--- utilities/scripts/check_and_restart_vbc_api_app.sh
+++ utilities/scripts/check_and_restart_vbc_api_app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-PID=$(~/.nvm/versions/node/v10.22.1/bin/pm2 pid vbc-appointment-select)
+PID=$(~/.nvm/versions/node/v10.22.1/bin/pm2 pid vbc-appointment-select) > /dev/null
 ~/.nvm/versions/node/v10.22.1/bin/pm2 describe vbc-appointment-select > /dev/null
 RUNNING=$?
-curl http://localhost:8080/api/v2/calendars > /dev/null
+curl http://localhost:8080/api/v2/calendars -s > /dev/null
 CALL_SUCCESSFUL=$?
 
 if [ "${RUNNING}" -ne 0 ] || [ "${PID}" -eq 0 ] || [ "${CALL_SUCCESSFUL}" -ne 0 ]; then
